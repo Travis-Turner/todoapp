d219a0331bed81477041ce91997b0da26d7d7147
richardturnerdeveloper <richardturnerdeveloper@gmail.com>
2017-11-05 21:49:36 -0500

    Add mongoose and local database

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost:27017/todoapp');
-// mongoose.Promise = global.Promise;
-//
-// module.exports = {
-//   mongoose
-// };
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/todoapp');
+mongoose.Promise = global.Promise;
+
+module.exports = {
+  mongoose
+};
